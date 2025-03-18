16edd28b5ca2281bfab5b1e0c157872312219fae
Jiang Sheng <jiangshengdev@outlook.com>
2025-03-18 21:37:30 +0800

    VueMacros

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
+import VueJsx from "@vitejs/plugin-vue-jsx";
+import VueMacros from "unplugin-vue-macros/vite";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [VueMacros({
+    shortEmits: true,
+
+    shortVmodel: {
+      prefix: "$"
+    },
+
+    reactivityTransform: true,
+    shortBind: true,
+
+    defineProp: {
+      edition: "kevinEdition"
+    },
+
+    defineEmit: true,
+    setupComponent: true,
+    setupSFC: true,
+    exportExpose: true,
+    exportRender: true,
+    chainCall: true,
+    jsxDirective: true,
+    booleanProp: true,
+
+    plugins: {
+      vue: vue({
+        include: [/\.vue$/, /\.setup\.[cm]?[jt]sx?$/]
+      }),
+
+      vueJsx: VueJsx()
+    }
+  })],
 })
